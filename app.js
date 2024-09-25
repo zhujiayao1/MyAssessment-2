4f720a79b45e078be24f851d8273618371293afc
zhujiayao1 <145834611+zhujiayao1@users.noreply.github.com>
2024-09-25 23:20:41 +0800

    Add files via upload
    构建node.js服务

---
+++ app.js
@@ -0,0 +1,27 @@
+//导入express框架
+const express = require("express");
+//创建web服务器
+const app = express();
+const path = require("path");
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "itheima",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, //有效时间一天
+    },
+  })
+);
+app.use(express.static(path.join(__dirname, "./public")));
+
+//导入路由
+const admin = require("./router/admin");
+app.use("/admin", admin);
+
+//启用监听
+app.listen(666, () => {
+  console.log("http://localhost:666/index.html");
+});
