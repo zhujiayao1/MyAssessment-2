bbfaf1873e07651f6761ca88f8d1d9e258b44d19
zhujiayao1 <145834611+zhujiayao1@users.noreply.github.com>
2024-09-30 01:45:58 +0800

    Update crowdfunding_db.sql

--- Part1/crowdfunding_db.sql
+++ Part1/crowdfunding_db.sql
@@ -1,3 +1,19 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80032 (8.0.32)
+ Source Host           : localhost:3306
+ Source Schema         : crowdfunding_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032 (8.0.32)
+ File Encoding         : 65001
+
+ Date: 30/09/2024 01:45:38
+*/
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -9,14 +25,15 @@ CREATE TABLE `category`  (
   `CATEGORY_ID` int NOT NULL AUTO_INCREMENT,
   `NAME` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`CATEGORY_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of category
 -- ----------------------------
-INSERT INTO `category` VALUES (7, 'Disaster Relief');
-INSERT INTO `category` VALUES (8, 'Animal Welfare');
-INSERT INTO `category` VALUES (9, 'Community Development');
+INSERT INTO `category` VALUES (1, 'medical');
+INSERT INTO `category` VALUES (7, 'crisis relief');
+INSERT INTO `category` VALUES (8, 'Animal');
+INSERT INTO `category` VALUES (9, 'social impact');
 
 -- ----------------------------
 -- Table structure for fundraiser
@@ -34,7 +51,7 @@ CREATE TABLE `fundraiser`  (
   PRIMARY KEY (`FUNDRAISER_ID`) USING BTREE,
   INDEX `CATEGORY_ID`(`CATEGORY_ID` ASC) USING BTREE,
   CONSTRAINT `fundraiser_ibfk_1` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of fundraiser
@@ -44,5 +61,6 @@ INSERT INTO `fundraiser` VALUES (2, 'Michael Lee', 'Animal Rescue Fund', 7000.00
 INSERT INTO `fundraiser` VALUES (3, 'Nina Patel', 'Building Community Gardens', 3000.00, 2500.00, 'Phoenix', 1, 9);
 INSERT INTO `fundraiser` VALUES (4, 'Oscar Wilde', 'Support for Homeless Families', 10000.00, 6000.00, 'Atlanta', 1, 9);
 INSERT INTO `fundraiser` VALUES (5, 'Penny Lane', 'Water Purification Project', 8000.00, 5000.00, 'Portland', 1, 7);
+INSERT INTO `fundraiser` VALUES (6, 'Jackson', 'Help The Jackson\'s Rebuild After Flood)', 100000.00, 7350.00, ' Byron Bay', 1, 7);
 
 SET FOREIGN_KEY_CHECKS = 1;
