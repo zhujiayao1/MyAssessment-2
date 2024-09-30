fa707f3a759b7a7b3f9a61e01e32d255dbc745b8
zhujiayao1 <145834611+zhujiayao1@users.noreply.github.com>
2024-10-01 02:38:02 +0800

    Update app.js
    search sql

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   //index.html
   res.sendFile(__dirname + '/Part3/index.html');
 });
-app.get('/search', (req, res) => {
+app.get('/search_page', (req, res) => {
   //index.html
   res.sendFile(__dirname + '/Part3/search.html');
 });
@@ -56,34 +56,48 @@ app.get('/fundraisers', (req, res) => {
 });
 
 
-//search by categorie name
-app.get('/search_by_cName', (req, res) => {
-  const categorieName = req.query.cName
-  const query = `
-	SELECT fundraiser.*
-	FROM fundraiser
-	JOIN category ON fundraiser.category_id = category.category_id
-	WHERE category.name = ?;
-  `;
-  connection.query(query, categorieName, (err, results) => {
+//search 
+app.get('/search', (req, res) => {
+  const { organizer, city, category } = req.query;
+  let query = 'SELECT * FROM fundraiser WHERE ';
+  let conditions = [];
+  let params = [];
+
+  if (organizer) {
+    conditions.push('organizer = ?');
+    params.push(organizer);
+  }
+  if (city) {
+    conditions.push('city = ?');
+    params.push(city);
+  }
+  if (category) {
+    conditions.push('category_id = (SELECT CATEGORY_ID FROM category WHERE name = ?)');
+    params.push(category);
+  }
+
+  query += conditions.join(' AND ');
+
+
+  connection.query(query, params, (err, results) => {
     if (err) throw err;
     res.json(results);
   });
 });
 
-//search by city
-app.get('/search_by_city', (req, res) => {
-  const city = req.query.city; 
-  const query = `
-    SELECT *
-    FROM fundraiser
-    WHERE city = ?;
-  `;
-  connection.query(query, city, (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// //search by city
+// app.get('/search_by_city', (req, res) => {
+//   const city = req.query.city; 
+//   const query = `
+//     SELECT *
+//     FROM fundraiser
+//     WHERE city = ?;
+//   `;
+//   connection.query(query, city, (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
 //search fundraiser by id
 app.get('/fundraiser/:id', (req, res) => {
